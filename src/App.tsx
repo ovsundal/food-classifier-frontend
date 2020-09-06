75f03bbe6715ab57c3cd932c0a72d7a18d625f6f
osund <osundal83@gmail.com>
2020-09-06 22:55:01 +0200

    remove boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
 
 function App() {
+
+  useEffect(() => {
+
+    const getData = async () =>  {
+      const result = await fetch(URL_ENDPOINT);
+
+      console.log(result);
+    }
+
+    getData();
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
